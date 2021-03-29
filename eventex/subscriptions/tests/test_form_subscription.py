a36b1b187d37e4a8af4a9792f4c62c4a1b58d310
Zandor S. Leal <zandorsabino@gmail.com>
2021-03-29 20:40:10 -0300

    Capitaliza os nomes

--- eventex/subscriptions/tests/test_form_subscription.py
+++ eventex/subscriptions/tests/test_form_subscription.py
@@ -4,10 +4,37 @@ from eventex.subscriptions.forms import SubscriptionForm
 
 
 class SubscriptionFormTest(TestCase):
-    def setUp(self):
-        self.form = SubscriptionForm()
-
     def test_form_has_fields(self):
         """Form must have 4 fields"""
+        form = SubscriptionForm()
         expected = ['name', 'cpf', 'email', 'phone']
-        self.assertEqual(expected, list(self.form.fields))
+        self.assertEqual(expected, list(form.fields))
+
+    def test_cpf_is_digit(self):
+        """CPF must only accept digits."""
+        form = self.make_validated_form(cpf='ABCD5678901')
+        self.assertFormErrorMessage(form, 'cpf', 'digits')
+
+    def test_cpf_has_11_digits(self):
+        """CPF must have 11 digits."""
+        form = self.make_validated_form(cpf='1234')
+        self.assertFormErrorMessage(form, 'cpf', 'length')
+
+    def test_name_must_be_captalized(self):
+        """Name must be capitalized"""
+        form = self.make_validated_form(name='ZANDOR leal')
+        self.assertEqual('Zandor Leal', form.cleaned_data['name'])
+
+    def assertFormErrorMessage(self, form, field, code):
+        errors = form.errors.as_data()
+        errors_list = errors[field]
+        exception = errors_list[0]
+        self.assertEqual(code, exception.code)
+
+    def make_validated_form(self, **kwargs):
+        valid = dict(name='Zandor Leal', cpf='12345678901',
+                    email='zandor@leal.com', phone='21-999999999')
+        data = dict(valid, **kwargs)
+        form = SubscriptionForm(data)
+        form.is_valid()
+        return form
