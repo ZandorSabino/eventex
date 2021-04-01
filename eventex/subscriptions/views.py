71025166ea2651e9ea67797bee90b39daf9b292c
Zandor S. Leal <zandorsabino@gmail.com>
2021-04-01 10:57:00 -0300

    Simplifica o forms.py

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -27,7 +27,7 @@ def create(request):
         return render(request, 'subscriptions/subscription_form.html',
                       {'form': form})
 
-    subscription = Subscription.objects.create(**form.cleaned_data)
+    subscription = form.save()
 
     # Send subscription email
     _send_mail('Confirmação de inscrição',
